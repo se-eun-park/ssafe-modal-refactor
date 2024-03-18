12e14f5dec9cc7f640e1d91591691c8a76eae789
ChaMin <kusoft1997@gmail.com>
2024-03-19 00:06:44 +0900

    Feat: 간편 로그인 닉네임 체크 로직 구현 #11

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,24 +1,31 @@
-import React, { SetStateAction,useState } from 'react';
+import React, { SetStateAction,useState,useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const LoginModal = ({ setModalOpen }: { setModalOpen: React.Dispatch<SetStateAction<boolean>> }) => {
-
   const navigate = useNavigate();
   const [nickName,setNickName]=useState(""); 
-  const [nickNameError,setNickNameError]=useState("false");
+  const [nickNameError,setNickNameError]=useState(false);
+  useEffect(()=>{
+    checkNickName();
+  },[nickName])
   const handleNextBtn=()=>{
     setModalOpen((prev)=>!prev);
     navigate('/myspace')
   }
   const checkNickName=()=>{
-    
+    if(nickName.length<4){
+      setNickNameError(false);
+    }
+    else{
+      setNickNameError(true);
+    }
   }
   return (
     <div className="relative w-[48rem] h-[23rem] rounded-md border-solid border-2 border-black-500 flex flex-col">
       <h1 className='text-[2rem] ps-[3.2rem] pt-[3.2rem] font-bold'>닉네임을 입력하세요</h1>
       <button className='text-[2rem] absolute top-0 right-3 font-bold' onClick={()=>setModalOpen((prev)=>!prev)}>X</button>
-      <h3 className='ps-[3.2rem] text-[1.4rem] text-orange-600'>ⓘ 닉네임은 최소 네 글자 이상이어야 합니다.</h3>
-      <input className='w-[41.6rem] h-[3.2rem] rounded-md mt-[3.2rem] mx-auto bg-[#F5F5F5]' placeholder='닉네임을 입력하세요'></input>
+      <h3 className={`ps-[3.2rem] text-[1.4rem] text-orange-600 ${nickNameError ? 'invisible' : ''}`} >ⓘ 닉네임은 최소 네 글자 이상이어야 합니다.</h3>
+      <input className='w-[41.6rem] h-[3.2rem] rounded-md mt-[3.2rem] mx-auto bg-[#F5F5F5]' placeholder='닉네임을 입력하세요' onChange={(e)=>setNickName(e.target.value)}></input>
       <button className='absolute bottom-2 right-3 w-[4rem] h-[3rem] mt-[3.2rem] text-[1.4rem] font-bold border-solid border-2 border-black-600/100' onClick={handleNextBtn}>확인</button>
         
     </div>
