9616557ae8b62e98db72dfeb3eb42cddbf7d9e59
ChaMin <kusoft1997@gmail.com>
2024-03-19 20:41:41 +0900

    Feat: Survey Modal 기능 구현 완료 #16

--- src/components/SurveyTag.tsx
+++ src/components/SurveyTag.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const SurveyTag = ({ tagText }: { tagText: string; }) => {
-    return (
-        <div className='w-[10rem] h-[3.2rem] bg-[#D4D4D4] grid place-items-center text-white rounded-md text-[1.2rem] font-bold mx-[0.8rem]'>
-          {tagText}
-        </div>
-    );
-  };
-  
-  export default SurveyTag;
-  
+const SurveyTag = ({ tagText, idx }: { tagText: string; idx: number }) => {
+  const colorList = ['bg-[#F04D1D]', 'bg-[#FF7364]', 'bg-[#A75EFF]', 'bg-[#1EBDFE]', 'bg-[#0DCF85]'];
+  console.log(colorList[idx]);
+  return (
+    <div
+      className={`w-[10rem] h-[3.2rem] ${colorList[idx]} grid place-items-center text-white rounded-md text-[1.2rem] font-bold mx-[0.8rem]`}
+    >
+      {tagText}
+    </div>
+  );
+};
+
+export default SurveyTag;
