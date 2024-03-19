6f21b5bcd238a1ab3ddd3dcfdf189d0d951e95ef
ChaMinXD <dvd3333@naver.com>
2024-03-19 17:20:16 +0900

    Feat: Survey Modal 태그 텍스트 추가 기능 구현 #16

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { SetStateAction, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import SurveyTag from '@components/SurveyTag';
 
 const Modal = ({
   modalType,
@@ -66,18 +67,37 @@ const LoginModal = ({ setModalOpen }: { setModalOpen: React.Dispatch<SetStateAct
 };
 
 const SurveyModal = ({ setModalOpen }: { setModalOpen: React.Dispatch<SetStateAction<boolean>> }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [tag, setTag] = useState('');
+  const [tagList, setTagList] = useState<string[]>([]);
+  const makeTag = () => {
+    
+    if (tag.trim() === '') {
+      return;
+    }
+    const newTagList = [...tagList, tag];
+    setTagList(newTagList);
+    setTag('');
+  }
   return (
-    <div className="relative w-[67.2rem] h-[52.5rem] rounded-md border-solid border-2 border-black-500 flex flex-col">
+    <div className="relative w-[67.2rem] h-[52.5rem] rounded-md border-solid border-2 border-black-500 flex flex-col bg-white">
       <button className='text-[2.5rem] absolute top-10 right-12 font-bold' onClick={() => setModalOpen((prev) => !prev)}>X</button>
       <h1 className='text-[2rem] ps-[4.5rem] pt-[3.2rem] font-bold'>새로운 설문지를 작성합니다.</h1>
       <h3 className='ps-[4.5rem] text-[1.4rem] text-gray-600' >새로운 설문지를 만들기 위한 설정입니다.</h3>
       <div className='w-[60.8rem] h-[33.6rem] flex flex-col rounded-md mx-auto mt-[2.4rem] bg-[#FAFAFA] border-solid border-2 border-black-500'>
         <label htmlFor="surveyTitle" className='ms-[1.6rem] mt-[1.6rem] text-[1.4rem]' >설문지 이름을 입력하세요</label>
-        <input id="surveyTitle" className='w-[57.6rem] h-[3.2rem] rounded-md mt-[0.8rem] mx-auto bg-[#F5F5F5] border-solid border-2 border-black-500' placeholder='제목을 입력하세요' ></input>
+        <input id="surveyTitle" className='w-[57.6rem] h-[3.2rem] rounded-md mt-[0.8rem] mx-auto bg-[#F5F5F5] border-solid border-2 border-black-500' onChange={(e) => setTitle(e.target.value)} placeholder='제목을 입력하세요' ></input>
         <label htmlFor="surveyContent" className='ms-[1.6rem] mt-[3.2rem] text-[1.4rem]'>설문지 설명을 입력하세요</label>
-        <textarea id='surveyContent' className='w-[57.6rem] h-[9.6rem] rounded-md mt-[0.8rem] mx-auto bg-[#F5F5F5] border-solid border-2 border-black-500' placeholder='내용을 입력하세요' ></textarea>
-        <div className='w-[57.6rem] h-[3.2rem] flex flex-row mx-auto mt-[3.2rem]'>
-          <div className='w-[10rem] h-[3.2rem] bg-[#D4D4D4] grid place-items-center text-white rounded-md text-[1.2rem] font-bold'>#태그입력</div>
+        <textarea id='surveyContent' className='w-[57.6rem] h-[9.6rem] rounded-md mt-[0.8rem] mx-auto bg-[#F5F5F5] border-solid border-2 border-black-500' onChange={(e) => setContent(e.target.value)} placeholder='내용을 입력하세요' ></textarea>
+        <div className='w-[57.6rem] h-[3.2rem] flex flex-row mx-auto mt-[3.2rem] justify-start' >
+          {tagList.map(
+            tag => (<SurveyTag tagText={tag}></SurveyTag>)
+          )}
+          <input className={`w-[10rem] h-[3.2rem] bg-[#D4D4D4] grid place-items-center text-white rounded-md text-[1.2rem] font-bold text-center ${tagList.length > 4 ? 'hidden' : ''}`} onChange={(e) => setTag(e.target.value)} onKeyDown={(e) => {
+            if (e.key === 'Enter')
+              makeTag()
+          }} maxLength={10} placeholder='#태그입력' value={tag}/>
         </div>
       </div>
       <button className='absolute bottom-10 right-12 w-[4rem] h-[3rem] mt-[3.2rem] rounded-md text-[1.4rem] font-bold border-solid border-2 border-black-600/100' >확인</button>
