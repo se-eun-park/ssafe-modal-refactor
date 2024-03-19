233aa7865f5328520e3d1a81fc63a51a45f6e0da
ChaMinXD <dvd3333@naver.com>
2024-03-19 16:14:01 +0900

    UI: Survey Modal 버튼 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -43,14 +43,15 @@ const LoginModal = ({ setModalOpen }: { setModalOpen: React.Dispatch<SetStateAct
       <button className='text-[2rem] absolute top-0 right-3 font-bold' onClick={()=>setModalOpen((prev)=>!prev)}>X</button>
       <h3 className={`ps-[3.2rem] text-[1.4rem] text-orange-600 ${nickNameError ? 'invisible' : ''}`} >ⓘ 닉네임은 최소 네 글자 이상이어야 합니다.</h3>
       <input className='w-[41.6rem] h-[3.2rem] rounded-md mt-[3.2rem] mx-auto bg-[#F5F5F5]' placeholder='닉네임을 입력하세요' onChange={(e)=>setNickName(e.target.value)}></input>
-      <button className='absolute bottom-2 right-3 w-[4rem] h-[3rem] mt-[3.2rem] text-[1.4rem] font-bold border-solid border-2 border-black-600/100' onClick={handleNextBtn}>확인</button>
+      <button className='absolute bottom-2 right-3 w-[4rem] h-[3rem] mt-[3.2rem] rounded-md text-[1.4rem] font-bold border-solid border-2 border-black-600/100' onClick={handleNextBtn}>확인</button>
     </div>
    
   );
 };
 const SurveyModal=({setModalOpen}:{ setModalOpen: React.Dispatch<SetStateAction<boolean>> }) => {
   return(
-            <div className="relative w-[67.2rem] h-[52.5rem] rounded-md border-solid border-2 border-black-500 flex flex-col">
+    <div className="relative w-[67.2rem] h-[52.5rem] rounded-md border-solid border-2 border-black-500 flex flex-col">
+            <button className='text-[2.5rem] absolute top-10 right-12 font-bold' onClick={()=>setModalOpen((prev)=>!prev)}>X</button>
             <h1 className='text-[2rem] ps-[4.5rem] pt-[3.2rem] font-bold'>새로운 설문지를 작성합니다.</h1>
             <h3 className='ps-[4.5rem] text-[1.4rem] text-gray-600' >새로운 설문지를 만들기 위한 설정입니다.</h3>
             <div className='w-[60.8rem] h-[33.6rem] flex flex-col rounded-md mx-auto mt-[2.4rem] bg-[#FAFAFA] border-solid border-2 border-black-500'>
@@ -61,8 +62,10 @@ const SurveyModal=({setModalOpen}:{ setModalOpen: React.Dispatch<SetStateAction<
             <div className='w-[57.6rem] h-[3.2rem] flex flex-row mx-auto mt-[3.2rem]'>
               <div className='w-[10rem] h-[3.2rem] bg-[#D4D4D4] grid place-items-center text-white rounded-md text-[1.2rem] font-bold'>#태그입력</div>
             </div>
-            </div>
-            </div>
+      </div>
+      <button className='absolute bottom-10 right-12 w-[4rem] h-[3rem] mt-[3.2rem] rounded-md text-[1.4rem] font-bold border-solid border-2 border-black-600/100' >확인</button>
+
+    </div>
      
   );
 };
