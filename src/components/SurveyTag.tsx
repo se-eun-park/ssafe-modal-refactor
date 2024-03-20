7975a49b9178fd40828ab8a20fc77fbc80818528
ChaMinXD <dvd3333@naver.com>
2024-03-20 09:55:56 +0900

    Fix: form data type 수정(convert any to form) , Survey modal props 수정

--- src/components/SurveyTag.tsx
+++ src/components/SurveyTag.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const SurveyTag = ({ tagText, idx }: { tagText: string; idx: number }) => {
   const colorList = ['bg-[#F04D1D]', 'bg-[#FF7364]', 'bg-[#A75EFF]', 'bg-[#1EBDFE]', 'bg-[#0DCF85]'];
-  console.log(colorList[idx]);
   return (
     <div
       className={`w-[10rem] h-[3.2rem] ${colorList[idx]} grid place-items-center text-white rounded-md text-[1.2rem] font-bold mx-[0.8rem]`}
